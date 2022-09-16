65e6734652e4d555441c1f723ebd2e29f95954c8
kames <kamescg@gmail.com>
2022-09-16 06:46:12 -0600

    v0.0.0-beta.0

--- test/MintableERC721.test.ts
+++ test/MintableERC721.test.ts
@@ -12,19 +12,21 @@ describe('ERC721KC', () => {
   let wallet2: SignerWithAddress;
   let ERC721KC: Contract;
   let ERC721KCFactory: ContractFactory;
+  let ERC20TWAB: Contract;
+  let ERC20TWABFactory: ContractFactory;
 
   before(async () => {
     [wallet0, wallet1, wallet2] = await getSigners();
+    ERC20TWABFactory = await ethers.getContractFactory('ERC20TWAB');
     ERC721KCFactory = await ethers.getContractFactory('ERC721KC');
   });
 
   beforeEach(async () => {
-    ERC721KC = await ERC721KCFactory.deploy('Collectable', 'NFT');
+    ERC20TWAB = await ERC20TWABFactory.deploy('Token', 'TOK');
+    ERC721KC = await ERC721KCFactory.deploy('Counterfactual', 'NFT', wallet0.address, ERC20TWAB.address);
   });
 
   describe('mint(address to, uint256 amount)', () => {
-    it('should SUCCEED to mint NFT #1', async () => {
-   
-    });
+    it('should SUCCEED to mint NFT #1', async () => {});
   });
 });
